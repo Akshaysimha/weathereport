24d8f9ea7f06ed0e319c341ee2d6527b9e946ac9
Akshaysimha <akshay.simha91@gmail.com>
2016-11-29 11:07:52 +0530

    display result in high chart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+    uri = URI.parse("http://api.openweathermap.org/data/2.5/forecast/city?q=bangalore,IN&APPID=3700a7e3d86bb9f7caa6b2ce98bacde2")
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    stats = JSON.parse(response.body)
+    @date = []
+    @temp = []
+    stats['list'].each do |s|
+      @date << s['dt_txt'].to_date
+      @temp << (s["main"]["temp"] / 10).round(2)
+    end
   end
 end
